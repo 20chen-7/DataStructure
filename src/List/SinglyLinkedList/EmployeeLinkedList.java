6c7b5f0d298aa7b62596e30264ae0aaa75628fd0
20chen-7 <102696148+20chen-7@users.noreply.github.com>
2022-12-25 10:22:46 -0700

    LinkedList and JDK LinkedList

---
+++ src/List/SinglyLinkedList/EmployeeLinkedList.java
@@ -0,0 +1,38 @@
+package List.SinglyLinkedList;
+
+public class EmployeeLinkedList {
+    private EmployeeNode head;
+    private int size;
+    public void addToFront(Employee employee){
+        EmployeeNode node = new EmployeeNode(employee);
+        node.setNext(head);
+        head = node;
+        size ++;
+    }
+    public EmployeeNode removeFromFront(){
+        if (isEmpty()){
+            return null;
+        }
+        EmployeeNode removedNode = head;
+        head = head.getNext();
+        size --;
+        removedNode.setNext(null); //clear makesure
+        return removedNode;
+    }
+    public boolean isEmpty(){
+        return head == null;
+    }
+    public void printList(){
+        EmployeeNode current = head;
+        System.out.println("HEAD -> ");
+        while (current != null){
+            System.out.print(current);
+            System.out.print(" -> ");
+            current = current.getNext();
+        }
+        System.out.println("null");
+    }
+    public int getSize(){
+       return size;
+    }
+}
